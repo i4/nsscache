424a573023a06c50fd8dad43174c12c42f7dada5
Simon Ruderich <simon@ruderich.org>
2019-06-11 09:48:05 +0200

    nss: tests: sort included headers

--- nss/tests/gr.c
+++ nss/tests/gr.c
@@ -18,11 +18,11 @@
  */
 
 #include <assert.h>
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <limits.h>
 
 #include "../cash_nss.h"
 
